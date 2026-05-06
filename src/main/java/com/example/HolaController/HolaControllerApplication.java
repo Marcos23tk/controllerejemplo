69f3ac1935509af302ca5d5c0a8642526bcb69e4
Marcos23tk <jhonmarcoslizanacondori@gmail.com>
2026-05-06 16:18:27 -0500

    Proyecto Spring Boot para Railway

---
+++ src/main/java/com/example/HolaController/HolaControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.HolaController;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolaControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HolaController/HolaControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HolaController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolaControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
